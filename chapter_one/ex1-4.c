0b3eee01e28134b8fec520fb6d5acce27b62232b
hunterxcobby <solomonsefah13@gmail.com>
2024-11-05 22:46:16 +0000

    Verify that the expression getchar() != EOF is 0 or 1

--- chapter_one/ex1-4.c
+++ chapter_one/ex1-4.c
@@ -1,32 +1,32 @@
 #include <stdio.h>
 
 /**
-  * Print Celsius-Farenheit table 
-  * for fahr = 0, 20, ..., 300; floating point version
-  */
+ * Print Celsius-Farenheit table 
+ * for fahr = 0, 20, ..., 300; floating point version
+ */
 
 int main(void)
 {
-    float fahr, celsius;
-    int lower, upper, step;
+	float fahr, celsius;
+	int lower, upper, step;
 
-    lower = 0;  /* lower limit of temperature table */
-    upper = 300;    /* upper limit */
-    step = 20;  /* step size */
-    
+	lower = 0;  /* lower limit of temperature table */
+	upper = 300;    /* upper limit */
+	step = 20;  /* step size */
 
-    /* Display Heading*/
-    printf("\nTemperature Conversion\n\n");
-    printf("Celcius\tFarenheit\n");
-    printf("_______\t________\n");
 
-    fahr = lower;
-    while (fahr <= upper)
-    {
-        celsius = (5.0/9.0) * (fahr-32.0);
-        printf("%5.1f\t%3.0f\n", celsius, fahr);
-        fahr = fahr + step;
-    }
+	/* Display Heading*/
+	printf("\nTemperature Conversion\n\n");
+	printf("Celcius\tFarenheit\n");
+	printf("_______\t________\n");
 
-    return(0);
+	fahr = lower;
+	while (fahr <= upper)
+	{
+		celsius = (5.0/9.0) * (fahr-32.0);
+		printf("%5.1f\t%3.0f\n", celsius, fahr);
+		fahr = fahr + step;
+	}
+
+	return(0);
 }
