5b5194d8e2638cbfcebba1131e32c82f1477a5e8
hunterxcobby <solomonsefah13@gmail.com>
2024-11-05 22:45:18 +0000

    * A program that copies its input to its output,  * replacing each string of one or more blanks  * by a single blank.

---
+++ chapter_one/ex1-9.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * a program that copies it input to its output,
+ * replacing each string of one or more blanks
+ * by a single blank
+ */
+
+int main(void)
+{
+    int c, nl, bl, tb;
+
+    nl = 0;
+    while ((c = getchar()) != EOF)
+    {
+
+    putchar(c);
+    } 
+    
+    return (0);
+}
