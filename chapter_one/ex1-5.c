261ac182071c0f2dbe7746df5101001641c7d76c
hunterxcobby <solomonsefah13@gmail.com>
2024-11-06 15:44:12 +0000

    fix betty

--- chapter_one/ex1-5.c
+++ chapter_one/ex1-5.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 
 /**
-  * Print Fahrenheit-Celsius table 
-  * in reverse order
-  * that is from 300 degrees to 0
-  */
+ * Print Fahrenheit-Celsius table
+ * in reverse order
+ * that is from 300 degrees to 0
+ */
 
 int main(void)
 {
-    int fahr;
+	int fahr;
 
-    /* Display Heading*/
-    printf("\nTemperature Conversion\n\n");
-    printf("Farenheit Celsius\n");
-    printf("_________ ______\n");
+	/* Display Heading*/
+	printf("\nTemperature Conversion\n\n");
+	printf("Farenheit Celsius\n");
+	printf("_________ ______\n");
 
-    for (fahr = 300; fahr >= 0; fahr = fahr - 20)
-    {
-        printf("%6d %9.1f\n", fahr, (5.0/9.0)*(fahr-32));
-    }
+	for (fahr = 300; fahr >= 0; fahr = fahr - 20)
+	{
+		printf("%6d %9.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
+	}
 
-    return(0);
+	return (0);
 }
