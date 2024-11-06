f97d2297f1708ecb0e52b4514096ab94cd5a3ed7
hunterxcobby <solomonsefah13@gmail.com>
2024-11-06 15:49:18 +0000

    fix betty

--- chapter_one/ex1-8.c
+++ chapter_one/ex1-8.c
@@ -6,26 +6,26 @@
 
 int main(void)
 {
-    int c, nl, bl, tb;
+	int c, nl, bl, tb;
 
-    nl = 0;
-    while ((c = getchar()) != EOF)
-    {
-        if (c == '\n' )
-        {
-            ++nl;
-        }
-        else if (c == ' ')
-        {
-            ++bl;
-        }
-        else if (c == '\t')
-        {
-            ++tb;
-        }
+	nl = 0;
+	while ((c = getchar()) != EOF)
+	{
+		if (c == '\n')
+		{
+			++nl;
+		}
+		else if (c == ' ')
+		{
+			++bl;
+		}
+		else if (c == '\t')
+		{
+			++tb;
+		}
 
-    printf("\n%dnewlines\n %d blanks \n %d tabs\n", nl, bl, tb);
-    } 
-    
-    return (0);
+		printf("\n%dnewlines\n %d blanks \n %d tabs\n", nl, bl, tb);
+	}
+
+	return (0);
 }
